037376355c40e20454c2c69c7f635b9da0261d9b
anapgil <ana.paula.gil@konfio.mx>
2016-12-22 12:29:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('Hi! I\'m Smooch Bot!')
+            return bot.say('¡Hola soy el Bot de Konfio!')
                 .then(() => 'askName');
         }
     },
 
     askName: {
-        prompt: (bot) => bot.say('What\'s your name?'),
+        prompt: (bot) => bot.say('¿Cómo te llamas?'),
         receive: (bot, message) => {
             const name = message.text;
             return bot.setProp('name', name)
